ced1346ac3ae0d56e53d60f6326e6869d03c9047
devrazz09 <hinarazelmarlonosera@gmail.com>
2025-09-09 20:45:32 +0800

    config

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,9 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  build: {
-    outDir: 'dist',
-  },
   plugins: [
     react(),
     tailwindcss()],
