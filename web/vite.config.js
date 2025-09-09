605b01906efaa000b61817351369479c9989d8b2
devrazz09 <hinarazelmarlonosera@gmail.com>
2025-09-09 20:54:11 +0800

    initial commit

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [
     react(),
     tailwindcss()],
+  base: '/',
+  build: {
+    outDir: 'dist'
+  }
 })
