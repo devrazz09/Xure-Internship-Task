3eb4178078bc507785d4fc3579e8afb189fba274
Razel Marlon Hina <115031242+devrazz09@users.noreply.github.com>
2025-09-11 11:03:36 +0800

    Update index.jsx

--- web/src/pages/exercise/exercise2/index.jsx
+++ web/src/pages/exercise/exercise2/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router"
 const Exercise2 = () => {
   return(
     <div className="w-full h-full flex justify-center items-center">
-      <div className="w-2/5 h2/5 border border-white rounded-md font-poppins-medium">
+      <div className="w-2/5 h-2/5 flex justify-center items-center gap-4 border border-white rounded-md font-poppins-medium">
         <div className="w-1/3 h-4/5 border border-white rounded-md">
           <Link to={"weatherapp"}>Weather App</Link>
         </div>
