a427a87693bf93cabfbd2361cf1a803905471773
Razel Marlon Hina <115031242+devrazz09@users.noreply.github.com>
2025-09-11 11:00:05 +0800

    Update index.jsx

--- web/src/pages/exercise/exercise2/index.jsx
+++ web/src/pages/exercise/exercise2/index.jsx
@@ -5,7 +5,7 @@ const Exercise2 = () => {
     <div className="w-full h-full flex justify-center items-center">
       <div className="w-2/5 h2/5 border border-white rounded-md font-poppins-medium">
         <div className="w-1/3 h-4/5 border border-white rounded-md">
-          <Link to{"weatherapp"}>Weather App</Link>
+          <Link to={"weatherapp"}>Weather App</Link>
         </div>
         <div className="w-1/3 h-4/5 border border-white rounded-md>
           <Link to={"ecommerce"}>E-commerce</Link>
