a1d2274893aa769bd9b8f5b30bea152d90a60e1e
Razel Marlon Hina <115031242+devrazz09@users.noreply.github.com>
2025-09-11 09:22:42 +0800

    Update index.jsx

--- web/src/pages/exercise/exercise1/index.jsx
+++ web/src/pages/exercise/exercise1/index.jsx
@@ -46,13 +46,7 @@ const Exercise1 = () => {
           </div>
           {data.map((item, index) => {
             const {firstName ,lastName ,age ,birthdate ,address ,middleName ,suffix} = item
-            return (
-              
-              <div className="w-full h-16 flex items-center justify-around hover:bg-gray-600 cursor-pointer" key={index}>
-                <div className="w-4/5 flex justify-around" 
-                onClick={()=>{
-                setModal(!modal)
-                setModalData({
+     setModalData({
                   firstName: firstName,
                   lastName: lastName,
                   middleName: middleName,
@@ -61,6 +55,12 @@ const Exercise1 = () => {
                   address: address,
                   birthdate: birthdate                  
                 })
+            return (
+              
+              <div className="w-full h-16 flex items-center justify-around hover:bg-gray-600 cursor-pointer" key={index}>
+                <div className="w-4/5 flex justify-around" 
+                onClick={()=>{
+                setModal(!modal)
               }} >
                   <p className="w-1/5 text-center text-md text-white font-poppins-medium">{`${firstName} ${middleName} ${lastName} ${suffix}`}</p>
                   <p className="w-1/5 text-center text-md text-white font-poppins-medium">{age}</p>
